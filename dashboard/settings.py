ad938851cb34819cb929ee3c76a9b1f464edc287
kimjy10 <kjy120924@gmail.com>
2022-01-26 17:17:00 +0900

    add secret_key in setting.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from my_settings import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,6 +24,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+SECRET_KEY = SECRET_KEY
 
 # Application definition
 
