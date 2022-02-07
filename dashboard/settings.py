989be47c263e846a68a5056b0d43456b1bb029c1
kang min sung <kangminsung@172-1-7-142.lightspeed.hstntx.sbcglobal.net>
2022-02-07 15:43:39 +0900

    Add: Tree 기능 구현

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'search',
     'summary',
     'weekly',
+    'trees',
 ]
 
 MIDDLEWARE = [
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('weekly', include('weekly.urls'))
+    path('weekly', include('weekly.urls')),
+    path('trees', include('trees.urls')),
 ]
