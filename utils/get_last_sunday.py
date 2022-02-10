6d9b9051a04943eb06ba7fe97c72ee9ccaf3589f
kimjy10 <kjy120924@gmail.com>
2022-02-10 14:34:16 +0900

    modified query parameter

--- utils/get_last_sunday.py
+++ utils/get_last_sunday.py
@@ -4,13 +4,11 @@ def get_last_sunday(date):
     date = datetime.strptime(date, "%Y-%m-%d")
     day_of_the_week = date.weekday()
 
-    last_sunday = date
-    if day_of_the_week < 6:
-        last_sunday = date - timedelta(days=(day_of_the_week+1))
-
+    day_of_sunday = 6
+    last_sunday = date + timedelta(days=(day_of_sunday-day_of_the_week))
     last_sunday = last_sunday.strftime("%Y-%m-%d")
 
     return last_sunday
 
 if __name__ == "__main__":
-    print(get_last_sunday("2022-01-30"))
+    print(get_last_sunday("2022-01-31"))
--- weekly/views/domain.py
+++ weekly/views/domain.py
@@ -9,6 +9,8 @@ from utils.connect_redshift import connect_redshift
 from utils.redshift_data import RedshiftData
 from utils.get_last_sunday import get_last_sunday
 from utils.get_previous_season import get_previous_season
+from utils.check_item import check_keys_in_dictionary
+
 
 class DomainTimeSeriesView(View):
 
@@ -44,13 +46,17 @@ ORDER BY end_date, domain1_nm
     @connect_redshift
     def get(self, request, *args, **kwargs):
         try:
+            required_keys = ["brand", "categories", "adult-kids", "start-date",
+                             "end-date-this-week", "seasons", "subcategories"]
+            check_keys_in_dictionary(request.GET, required_keys)
+
             brand = request.GET["brand"]
-            category = request.GET["category"]
-            adult_kids = request.GET["adult_kids"]
-            start_date = request.GET["start_date"]
-            end_date_this_week = request.GET["end_date_this_week"]
-            season = request.GET.getlist("season", None)
-            sub_category = request.GET.getlist("sub_category", None)
+            category = request.GET["categories"]
+            adult_kids = request.GET["adult-kids"]
+            start_date = request.GET["start-date"]
+            end_date_this_week = request.GET["end-date-this-week"]
+            season = request.GET.getlist("seasons", None)
+            sub_category = request.GET.getlist("subcategories", None)
             connect = request.connect
 
             end_date_this_week = get_last_sunday(end_date_this_week)
@@ -222,14 +228,18 @@ ORDER BY week_sale_amt_cy DESC, domain1_nm
     @connect_redshift
     def get(self, request, *args, **kwargs):
         try:
+            required_keys = ["brand", "categories", "adult-kids", "start-date",
+                             "end-date", "end-date-this-week", "seasons", "subcategories"]
+            check_keys_in_dictionary(request.GET, required_keys)
+
             brand = request.GET["brand"]
-            category = request.GET["category"]
-            adult_kids = request.GET["adult_kids"]
-            start_date = request.GET["start_date"]
-            end_date = request.GET["end_date"]
-            end_date_this_week = request.GET["end_date_this_week"]
-            season = request.GET.getlist("season",None)
-            sub_category = request.GET.getlist("sub_category",None)
+            category = request.GET["categories"]
+            adult_kids = request.GET["adult-kids"]
+            start_date = request.GET["start-date"]
+            end_date = request.GET["end-date"]
+            end_date_this_week = request.GET["end-date-this-week"]
+            season = request.GET.getlist("seasons",None)
+            sub_category = request.GET.getlist("subcategories",None)
             connect =request.connect
 
             end_date_this_week = get_last_sunday(end_date_this_week)
