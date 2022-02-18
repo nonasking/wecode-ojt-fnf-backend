48a99a1cfa27b7eab2aa764b61da77dc77fbf7f5
kimjy10 <kjy120924@gmail.com>
2022-02-18 15:50:59 +0900

    add feature for top 20 summary

--- top20/urls.py
+++ top20/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from top20.views import (Top20SummaryView,
-                         Top20ListView)
+                         Top20ListView,
+                         Top20TotalSummaryView)
 
 urlpatterns = [
     path('/summary', Top20SummaryView.as_view()),
     path('/list', Top20ListView.as_view()),
+    path('/total-summary', Top20TotalSummaryView.as_view()),
 ]
--- top20/views.py
+++ top20/views.py
@@ -154,6 +154,9 @@ order by rk
             redshift_data = RedshiftData(connect, query)
             data = redshift_data.get_data()
 
+            if data is None:
+                return JsonResponse({"message":"QUERY_ERROR","query":query}, status=400)
+
             columns = [
                 {"title":item[0], 
                  "field":item[1],
@@ -368,6 +371,9 @@ limit {para_rank_limit}
             redshift_data = RedshiftData(connect, query)
             data = redshift_data.get_data()
 
+            if data is None:
+                return JsonResponse({"message":"QUERY_ERROR","query":query}, status=400)
+
             columns = [
                 {"title":item[0],
                  "field":item[1],
@@ -385,4 +391,124 @@ limit {para_rank_limit}
             return JsonResponse({"message":getattr(e, "message",str(e))}, status=400)
 
 
+class Top20TotalSummaryView(View):
 
+    def __init__(self):
+        self.column_descriptions = [
+            ["cls", "총합계", False],
+            ["sale_amt_kor_ttl", "판매액", "money"],
+            ["sale_qty_kor_ttl", "판매량", "money"],
+            ["asp", "실판가", "money"], 
+            ["sale_qty_kor_retail", "국내", "money"],
+            ["sale_qty_kor_duty", "면세", "money"],
+            ["sale_qty_kor_rfwholesale", "RF도매", "money"],
+            ["wh_stock_qty_kor", "물류재고", "money"],
+            ["stock_qty_kor", "총재고", "money"],
+            ["woi", "재고주수", False],
+        ]
+
+    def get_query(self, *args, **kwargs):
+        query = """
+select '총합계' cls
+     , sale_amt / 1000000     as sale_amt_kor_ttl
+     , sale_qty                as sale_qty_kor_ttl
+     , case when sale_qty = 0 then 0 else sale_amt / sale_qty end asp
+     , sale_qty_rtl            as sale_qty_kor_retail
+     , sale_qty_notax        as sale_qty_kor_duty
+     , sale_qty_rfdome        as sale_qty_kor_rfwholesale
+     , sale_qty_dutyrfdome    as sale_qty_kor_dutyrfwholesale
+     , wh_stock_qty            as wh_stock_qty_kor
+     , stock_qty            as stock_qty_kor
+     , case when sale_qty = 0 then stock_qty else stock_qty / sale_qty end woi
+from (
+    select sum(sale_nml_sale_amt_cns+sale_ret_sale_amt_cns) sale_amt
+         , sum(sale_nml_qty_cns+sale_ret_qty_cns) sale_qty
+         , sum(sale_nml_qty_rtl+sale_ret_qty_rtl) sale_qty_rtl
+         , sum(sale_nml_qty_notax+sale_ret_qty_notax) sale_qty_notax
+         , sum(sale_nml_qty_rf+sale_ret_qty_rf+sale_nml_qty_dome+sale_ret_qty_dome) sale_qty_rfdome
+         , sum(sale_nml_qty_rf+sale_ret_qty_rf+sale_nml_qty_dome+sale_ret_qty_dome+sale_nml_qty_notax+sale_ret_qty_notax) sale_qty_dutyrfdome
+         , sum(wh_stock_qty)    as wh_stock_qty
+         , sum(stock_qty) as stock_qty
+    from prcs.db_scs_w a, prcs.db_prdt b
+    where a.prdt_cd = b.prdt_cd
+    and b.brd_cd = '{para_brand}'
+    and a.end_dt = '{para_end_dt}'
+    AND sub_cat_nm IN {para_sub_category}
+    AND domain1_nm IN {para_domain}
+    AND (a.sesn||'_'||sesn_sub_nm) IN {para_season}
+    AND item IN {para_item}
+    AND (prdt_nm LIKE '%{para_search_keyword}%' OR style_cd LIKE '%{para_search_keyword}%')
+    AND adult_kids_nm IN {para_adult_kids}
+)a
+        """.format(
+            para_brand=kwargs['brand'],
+            para_sub_category=kwargs['sub_category'],
+            para_domain=kwargs['domain'],
+            para_item=kwargs['item'],
+            para_season=kwargs['season'],
+            para_search_keyword=kwargs['search_keyword'],
+            para_adult_kids=kwargs['adult_kids'],
+            para_rank_limit=kwargs['rank_limit'],
+            para_end_dt=kwargs["end_date_this_week"],
+        )
+        
+        return query
+
+    @connect_redshift
+    def get(self, request, *args, **kwargs):
+        try:
+            brand = request.GET["brand"]
+            end_date_this_week = request.GET["end-date"]
+            search_keyword = request.GET.get("search_keyword", None)
+            rank_limit = request.GET.get("limit", 200)
+            adult_kids = request.GET["adult-kids"].split(",")
+            item = request.GET["items"].split(",")
+            domain = request.GET["domains"].split(",")
+            season = request.GET["seasons"].split(",")
+            sub_category = request.GET["subcategories"].split(",")
+            connect = request.connect
+
+            end_date_this_week = get_last_sunday(end_date_this_week)
+
+            adult_kids = get_tuple(adult_kids)
+            item = get_tuple(item)
+            domain = get_tuple(domain)
+            season = get_tuple(season)
+            sub_category = get_tuple(sub_category)
+
+            if search_keyword is None:
+                search_keyword = ""
+
+            query = self.get_query(
+                brand = brand,
+                sub_category = sub_category,
+                domain = domain,
+                item = item,
+                season = season,
+                search_keyword = search_keyword,
+                adult_kids = adult_kids,
+                end_date_this_week = end_date_this_week,
+                rank_limit = rank_limit
+            )
+
+            redshift_data = RedshiftData(connect, query)
+            data = redshift_data.get_data()
+
+            if data is None:
+                return JsonResponse({"message":"QUERY_ERROR","query":query}, status=400)
+
+            columns = [
+                {"title":item[0],
+                 "field":item[1],
+                 "formatter":item[2],
+                 "formatterParams":{"precision":False}}
+                for item in self.column_descriptions
+            ]
+
+            column_list = [item[0] for item in self.column_descriptions]
+            contents = data[column_list].to_dict("records")
+
+            return JsonResponse({"message":"SUCCESS", "columns":columns, "data":contents}, status=200)
+
+        except KeyError as e:
+            return JsonResponse({"message":getattr(e, "message",str(e))}, status=400)
